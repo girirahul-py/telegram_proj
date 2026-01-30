ff9bab993c9366bc9cab56936750267766c07c60
girirahul-py <rahul.fortmindzpy@gmail.com>
2026-01-30 16:21:50 +0530

    FEAT : STRUCTURE WITH FILE ADDED IN PROJECT

---
+++ app/main.py
@@ -0,0 +1,33 @@
+#app/main.py
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.api.routers import router 
+from app.core.config import GITHUB_TOKEN
+
+## --- Initialize FastAPI Application ---
+
+app = FastAPI(
+    title="GitHub Repository Control",
+    description="Permission-Aware Multi-Repo Deployment Automation Platform",
+    version="1.0.0",
+)
+
+## --- Middleware ---
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+## --- Include API Router ---
+
+app.include_router(router, prefix="/api/v1")
+
+
+## --- Root Endpoint ---
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the GitHub Repository Control API"}
